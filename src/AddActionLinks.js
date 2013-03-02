4472f69532bd47155b1e2986c9dc655cdf2f1bae
Helder <he7d3r@gmail.com>
2013-03-02 16:12:18 +0000

    Script to add "edit", "hist" and "delete" links to WhatLinksHere and templates used in the page
    Applies to
    * https://pt.wikibooks.org/wiki/Special:WhatLinksHere
    * The links in the bottom of the edit box
    Based on revision 2 of
    https://www.mediawiki.org/wiki/Snippets/Special:WhatLinksHere_action_links
    (created by https://www.mediawiki.org/wiki/User:Krinkle)
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/AddActionLinks.js
@@ -0,0 +1,52 @@
+/**
+ * Adds "edit", "hist" and "delete" link to [[Special:WhatLinksHere]] and the list of templates used in a page
+ * @author: [[User:Helder.wiki]]
+ * Based on [[mw:Snippets/Special:WhatLinksHere action links]] (rev 2)
+ * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/AddActionLinks.js]] ([[File:User:Helder.wiki/Tools/AddActionLinks.js]])
+ */
+/*jslint browser: true, white: true, forin: true, todo: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+
+function addRelevantLinks ( rights ){
+	var actions = {
+			history: 'hist'
+		},
+		sel = {};
+	if ( $.inArray( 'delete', rights ) !== -1 ){
+		actions['delete'] = 'elim';
+	}
+	if( mw.config.get( 'wgCanonicalSpecialPageName' ) ){
+		actions.edit = 'editar';
+		sel.items = '#mw-whatlinkshere-list li';
+		sel.link = '.mw-whatlinkshere-tools a:last';
+	} else {
+		sel.items = '#editform .templatesUsed li';
+		sel.link = 'a:last';
+	}
+	$( function(){
+		$( sel.items ).each( function() {
+			var action,
+				$a = $( sel.link, this ),
+				url = mw.config.get( 'wgScript' ) + '?title=' +
+					encodeURIComponent( $( 'a:first', this ).text() ) +
+					'&action=';
+			for( action in actions ){
+				$a.after(
+					$( '<a>' )
+						.attr( 'href', url + action )
+						.text( actions[ action ] )
+				).after( ' | ' );
+			}
+		} );
+	} );
+
+}
+
+if( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Whatlinkshere' || $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
+	// TODO: Add chekboxes to show/hide each extra button
+	mw.user.getRights( addRelevantLinks );
+}
+
+}( mediaWiki, jQuery ) );
